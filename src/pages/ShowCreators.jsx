559c0befd0e87c3ff7ed10c5b2be2ec151d01452
sahrishA <159168795+sahrishA@users.noreply.github.com>
2024-08-31 18:41:19 -0400

    Update ShowCreators.jsx

--- src/pages/ShowCreators.jsx
+++ src/pages/ShowCreators.jsx
@@ -1,41 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { supabase } from '../client';
-import CreatorCard from '../components/CreatorCard';
+import CreatorCard from '../components/creatorCard';
 
 const ShowCreators = () => {
   const [creators, setCreators] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchCreators = async () => {
-      const { data, error } = await supabase.from('creators').select('*');
-      if (error) {
-        console.error(error);
-      } else {
-        setCreators(data);
-      }
+      const { data } = await supabase.from('creators').select('*');
+      setCreators(data);
     };
 
     fetchCreators();
   }, []);
 
-  const handleDelete = (id) => {
-    setCreators(creators.filter(creator => creator.id !== id));
-  };
-
   return (
     <div>
       <h1>Content Creators</h1>
+      <div style={{ marginBottom: '20px' }}>
+        <button onClick={() => navigate('/add-creator')}>Add Creator</button>
+
+      </div>
       <div className="creators-list">
         {creators.length > 0 ? (
           creators.map((creator) => (
             <CreatorCard
               key={creator.id}
-              id={creator.id}
               name={creator.name}
               url={creator.url}
               description={creator.description}
               imageURL={creator.imageURL}
-              onDelete={handleDelete}  // Pass the delete handler
             />
           ))
         ) : (
@@ -47,4 +43,5 @@ const ShowCreators = () => {
 };
 
 export default ShowCreators;
+
 
